256f3d557cbc3dd5aebed54537085240ba22dbc6
shoberg44 <steven.hoberg23@d155.org>
2022-10-11 07:56:45 -0500

    Initial Commit

---
+++ burgerStore/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  burgerStore
+//
+//  Created by STEVEN HOBERG on 10/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
