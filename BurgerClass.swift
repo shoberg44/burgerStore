4ba2acfa690503580175a2c49cc88f645da17867
shoberg44 <steven.hoberg23@d155.org>
2022-10-17 08:51:11 -0500

    funnyFish.jpg

--- BurgerClass.swift
+++ BurgerClass.swift
@@ -8,7 +8,10 @@
 import Foundation
 public class Burger{
     enum btype{
-        case cheese,plain,custom
+        case cheese,plain,custom,deluxe
+    }
+    enum burgerDone{
+        case blueRare, rare, mediumRare, medium, mediumWell, wellDone
     }
     var ketchup: Bool
     var patty: Int
@@ -19,7 +22,8 @@ public class Burger{
     var tomato: Bool
     var onion: Bool
     var bacon: Bool
-    init(ketchup: Bool, patty: Int, bun: Bool, bbqsauce: Bool, mushroom: Bool, lettuce: Bool, tomato: Bool, onion: Bool, bacon: Bool){
+    var mayo: Bool
+    init(ketchup: Bool, patty: Int, bun: Bool, bbqsauce: Bool, mushroom: Bool, lettuce: Bool, tomato: Bool, onion: Bool, bacon: Bool, mayo: Bool){
         self.ketchup = ketchup // self refers to global level varable
         self.patty = patty
         self.bun = bun
@@ -29,5 +33,19 @@ public class Burger{
         self.tomato = tomato
         self.onion = onion
         self.bacon = bacon
+        self.mayo = mayo
     }
+    init(){
+        ketchup = false
+        patty = 1
+        bun = false
+        bbqsauce = false
+        mushroom = false
+        lettuce = false
+        tomato = false
+        onion = false
+        bacon = false
+        mayo = false
+    }
+    
 }
